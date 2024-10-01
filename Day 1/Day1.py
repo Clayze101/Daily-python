af8444573abf99c0809a370fb562fb3d986f95fe
Ganesh Aakunoor <122515623+Clayze101@users.noreply.github.com>
2024-10-01 19:11:42 +0530

    Add files via upload

---
+++ Day 1/Day1.py
@@ -0,0 +1,11 @@
+# print("Hello World!!")
+
+# print("Hello World!!\nHello world\nHello World!!")
+
+# print("Hello " + input("Whats is your name?") + "!")
+
+# print(len(input("Your name is?")))
+
+username = input("Enter your username: ")
+length = len(str(username))
+print(length)
